86bb82d8e2ed3ad786901c2ef6591c677f5c9f76
Sahil Thakur <mesahilthakur@gmail.com>
2023-09-23 23:57:21 +0530

    results added

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,29 @@
+import Results from "@/components/Results";
 
-export default function Home() {
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({searchParams}) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 100000 } }
+  );
+
+  // if (!res.ok) {
+  //   console.log(Error)
+  //   throw new Error("Failed to load movies");
+  // }
+  const data = res.json();
+  const results = data.results;
+
   return (
     <div>
-      <h1 className="text-center text-red-400 bg-red-500">hi</h1>
+      {/* <h1 className="text-center text-red-400 bg-red-500">
+        hi
+      </h1> */}
+      <Results results={results} />
     </div>
-  )
+  );
 }
